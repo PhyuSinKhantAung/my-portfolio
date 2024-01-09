f1e9bc87cd53964454a3c7165b80a4f4e8ad0da1
phyusin <rollyphyusinkhant7290@gmail.com>
2024-01-09 16:21:43 +0630

    work in progress

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,18 @@
 'use client';
 import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
+import AboutPage from './about/page';
+import Projects from '@/components/Projects';
+import Contact from '@/components/Contact';
 
 export default function Home() {
   const router = useRouter();
 
-  useEffect(() => {
-    router.push('/about');
-  }, []);
-
-  return null;
+  return (
+    <>
+      <AboutPage />
+      <Projects />
+      <Contact />
+    </>
+  );
 }
